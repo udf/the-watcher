b151e50c9fa3e5aa52c35a5c8a17629e2d54d789
udf <tabhooked@gmail.com>
2021-12-15 20:31:27 +0200

    what's up, set? me.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = '0.1'
+
+setup(
+  name='watcher-bot',
+  version=VERSION,
+  packages=['watcher-bot'],
+  install_requires=[
+    'telethon',
+    'aiohttp'
+  ]
+)
