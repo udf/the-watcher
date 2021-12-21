a959493b68d71034dc42e4ee1ea186f865c2666b
udf <tabhooked@gmail.com>
2021-12-22 01:23:37 +0200

    include plugins

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from glob import glob
 
 VERSION = '0.1.3'
 
@@ -8,6 +9,9 @@ setup(
   name='watcher-bot',
   version=VERSION,
   packages=find_packages(),
+  data_files=[
+    ('watcher-bot/plugins', glob('watcher-bot/plugins/*.py'))
+  ],
   install_requires=[
     'telethon',
     'aiohttp'
