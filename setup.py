b7fc0e94d50347e970871516750761d62518a004
udf <tabhooked@gmail.com>
2021-12-22 01:29:35 +0200

    ur a module now harry

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from glob import glob
 
 VERSION = '0.1.3'
 
 setup(
   name='watcher-bot',
   version=VERSION,
-  packages=find_packages(),
-  data_files=[
-    ('watcher-bot/plugins', glob('watcher-bot/plugins/*.py'))
-  ],
+  packages=find_packages() + ['watcher-bot.plugins'],
   install_requires=[
     'telethon',
     'aiohttp'
