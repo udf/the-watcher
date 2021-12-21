12f0eef5533b556faf420e03a79a3dde37ee4937
udf <tabhooked@gmail.com>
2021-12-22 00:57:19 +0200

    add all modules

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 VERSION = '0.1.3'
 
 setup(
   name='watcher-bot',
   version=VERSION,
-  packages=['watcher-bot'],
+  packages=find_packages(),
   install_requires=[
     'telethon',
     'aiohttp'
