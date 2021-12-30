ec770a261b36fc10eba261498fd6c5ea9cca5bff
udf <tabhooked@gmail.com>
2021-12-31 00:09:18 +0200

    i have brain damage

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from glob import glob
 
 VERSION = '0.1.3'
 
 setup(
   name='watcher-bot',
   version=VERSION,
-  packages=find_packages(),
-  data_files=[
-    ('plugins', glob('watcher-bot/plugins/**/*.py', recursive=True))
-  ],
+  packages=find_packages() + find_packages('watcher-bot/plugins') + ['watcher-bot.plugins'],
   install_requires=[
     'telethon',
     'aiohttp'
